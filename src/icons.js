2baad251509746dd3db2214d5e7cb13c5fa4f5cd
Jasurbek-Creative <JasurbekJalilov888@gmail.com>
2024-03-01 21:36:34 +0500

    create icon js

---
+++ src/icons.js
@@ -0,0 +1,9 @@
+export const rightIcon = (<svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.88916 2L9.11022 9.22107L1.88916 16.4421" stroke="#EFF0F6" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+)
+
+export const leftIcon = (<svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.11084 2L1.88978 9.22107L9.11084 16.4421" stroke="#E8E8F6" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+)
